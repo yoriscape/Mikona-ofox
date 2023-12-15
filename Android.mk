19c52492759742a653d802a2818a1d5e4263a64f
Darth9 <reyprimerjefe@gmail.com>
2023-12-15 12:11:24 +0000

    Tidy up formatting, and add copyright information

--- Android.mk
+++ Android.mk
@@ -3,9 +3,13 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+# Copyright (C) 2021-2023 The OrangeFox Recovery Project
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
 
 LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter alioth munch,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
+#
--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,11 +1,15 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+# Copyright (C) 2021-2023 The OrangeFox Recovery Project
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_alioth.mk \
+	$(LOCAL_DIR)/twrp_alioth.mk \
 	$(LOCAL_DIR)/twrp_munch.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_alioth-eng \
+	twrp_alioth-eng \
 	twrp_munch-eng
+#
