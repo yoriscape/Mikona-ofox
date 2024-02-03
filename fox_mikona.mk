7dc36702a2b9348dde5114d536f77c9c9763a240
Darth9 <reyprimerjefe@gmail.com>
2024-02-03 09:49:19 +0000

    Update settings relating to data format

--- fox_mikona.mk
+++ fox_mikona.mk
@@ -1,7 +1,6 @@
-
 #
 #	This file is part of the OrangeFox Recovery Project
-# 	Copyright (C) 2023 The OrangeFox Recovery Project
+# 	Copyright (C) 2023-2024 The OrangeFox Recovery Project
 #
 #	OrangeFox is free software: you can redistribute it and/or modify
 #	it under the terms of the GNU General Public License as published by
@@ -30,21 +29,33 @@ OF_CLOCK_POS := 1
 # other stuff
 OF_IGNORE_LOGICAL_MOUNT_ERRORS := 1
 OF_USE_GREEN_LED := 0
-
 OF_QUICK_BACKUP_LIST := /boot;/data;
-
 OF_ENABLE_LPTOOLS := 1
 OF_NO_TREBLE_COMPATIBILITY_CHECK := 1
-
-# ensure that /sdcard is bind-unmounted before f2fs data repair or format
-OF_UNBIND_SDCARD_F2FS := 1
-
-# no OTA menu
-OF_DISABLE_OTA_MENU := 1
 
 # full size
 OF_DYNAMIC_FULL_SIZE := 9126805504
 
 # number of list options before scrollbar creation
 OF_OPTIONS_LIST_NUM := 6
+
+# ----- data format stuff -----
+# ensure that /sdcard is bind-unmounted before f2fs data repair or format
+OF_UNBIND_SDCARD_F2FS := 1
+
+# automatically wipe /metadata after data format
+OF_WIPE_METADATA_AFTER_DATAFORMAT := 1
+
+# alioth
+ifeq ($(PRODUCT_RELEASE_NAME),alioth)
+  # avoid MTP issues after data format
+  OF_BIND_MOUNT_SDCARD_ON_FORMAT := 1
+
+  # refresh encryption props before formatting data
+#  OF_REFRESH_ENCRYPTION_PROPS_BEFORE_FORMAT := 1
+
+  ifeq ($(FOX_VENDOR_BOOT_RECOVERY),)
+      OF_USE_LZ4_COMPRESSION := 1
+  endif
+endif
 #
